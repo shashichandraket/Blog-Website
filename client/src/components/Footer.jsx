87c2b2e29bef7425739cad3106d35d88af94778d
shashichandraket <shashiyadavkhg123@gmail.com>
2024-02-15 06:15:31 +0530

    changing website's name

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -18,7 +18,7 @@ export default function FooterCom() {
               className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
             >
               <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-                Sahand's
+                Shashi's
               </span>
               Blog
             </Link>
@@ -39,7 +39,7 @@ export default function FooterCom() {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  Sahand's Blog
+                  Shashi's Blog
                 </Footer.Link>
               </Footer.LinkGroup>
             </div>
@@ -47,7 +47,7 @@ export default function FooterCom() {
               <Footer.Title title="Follow us" />
               <Footer.LinkGroup col>
                 <Footer.Link
-                  href="https://www.github.com/sahandghavidel"
+                  href="https://www.github.com/shashichandraket"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
@@ -69,7 +69,7 @@ export default function FooterCom() {
         <div className="w-full sm:flex sm:items-center sm:justify-between">
           <Footer.Copyright
             href="#"
-            by="Sahand's blog"
+            by="Shashi's blog"
             year={new Date().getFullYear()}
           />
           <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
@@ -77,7 +77,7 @@ export default function FooterCom() {
             <Footer.Icon href="#" icon={BsInstagram} />
             <Footer.Icon href="#" icon={BsTwitter} />
             <Footer.Icon
-              href="https://github.com/sahandghavidel"
+              href="https://github.com/shashichandraket"
               icon={BsGithub}
             />
             <Footer.Icon href="#" icon={BsDribbble} />
